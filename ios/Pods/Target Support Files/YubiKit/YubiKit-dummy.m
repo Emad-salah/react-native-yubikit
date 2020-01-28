774fbf1e58741cef0efd297ac5e0bc60151f51d6
emad-salah <emads14@gmail.com>
2020-01-28 16:42:25 +0100

    iOS folder added

---
+++ ios/Pods/Target Support Files/YubiKit/YubiKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YubiKit : NSObject
+@end
+@implementation PodsDummy_YubiKit
+@end
---
+++ ios/RNYubikit.swift
@@ -0,0 +1,9 @@
+import Yubikit
+
+@objc(RNYubikit)
+class RNYubikit: NSObject {
+  @objc 
+  func initYubikit(_ resolve: @escaping RCTPromiseResolveBlock, _ reject: @escaping RCTPromiseRejectBlock) {
+    
+  }
+}
