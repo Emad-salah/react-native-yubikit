0e249261f24074264ca3e4c945ad554595f5c185
emad-salah <emads14@gmail.com>
2020-01-29 12:19:49 +0100

    Objective-C method exports added

--- ios/RNYubikit.m
+++ ios/RNYubikit.m
@@ -2,7 +2,25 @@
 
 @interface RCT_EXTERN_MODULE(RNYubikit, NSObject)
 
-RCT_EXPORT_METHOD(initYubikit
+RCT_EXPORT_METHOD(initNFCSession
+                  : (RCTPromiseResolveBlock)resolve
+                  : (RCTPromiseRejectBlock)reject)
+
+RCT_EXPORT_METHOD(initAccessorySession
+                  : (RCTPromiseResolveBlock)resolve
+                  : (RCTPromiseRejectBlock)reject)
+
+RCT_EXPORT_METHOD(stopNFCSession
+                  : (RCTPromiseResolveBlock)resolve
+                  : (RCTPromiseRejectBlock)reject)
+
+RCT_EXPORT_METHOD(stopAccessorySession
+                  : (RCTPromiseResolveBlock)resolve
+                  : (RCTPromiseRejectBlock)reject)
+
+RCT_EXPORT_METHOD(registerU2F
+                  : (NSString *) challenge
+                  : (NSString *) appId
                   : (RCTPromiseResolveBlock)resolve
                   : (RCTPromiseRejectBlock)reject)
 
