626178179997b6f914ffa6315d4e3d7495462528
emad-salah <emads14@gmail.com>
2020-01-29 18:06:06 +0100

    Fixes

--- ios/RNYubikit.m
+++ ios/RNYubikit.m
@@ -1,27 +1,7 @@
 #import <React/RCTBridgeModule.h>
 
 @interface RCT_EXTERN_MODULE(RNYubikit, NSObject)
+  RCT_EXTERN_METHOD(initNFCSession:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 
-RCT_EXPORT_METHOD(initNFCSession
-                  : (RCTPromiseResolveBlock)resolve
-                  : (RCTPromiseRejectBlock)reject)
-
-RCT_EXPORT_METHOD(initAccessorySession
-                  : (RCTPromiseResolveBlock)resolve
-                  : (RCTPromiseRejectBlock)reject)
-
-RCT_EXPORT_METHOD(stopNFCSession
-                  : (RCTPromiseResolveBlock)resolve
-                  : (RCTPromiseRejectBlock)reject)
-
-RCT_EXPORT_METHOD(stopAccessorySession
-                  : (RCTPromiseResolveBlock)resolve
-                  : (RCTPromiseRejectBlock)reject)
-
-RCT_EXPORT_METHOD(registerU2F
-                  : (NSString *) challenge
-                  : (NSString *) appId
-                  : (RCTPromiseResolveBlock)resolve
-                  : (RCTPromiseRejectBlock)reject)
-
+  RCT_EXTERN_METHOD(initAccessorySession:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 @end
