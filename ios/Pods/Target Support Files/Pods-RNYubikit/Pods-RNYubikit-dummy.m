9c66414e02f3a2a3dd60997769f85cfa3d82fb45
emad-salah <emads14@gmail.com>
2020-01-29 12:45:48 +0100

    React Native iOS bug fixes

--- ios/Pods/Target Support Files/Pods-RNYubikit/Pods-RNYubikit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RNYubikit : NSObject
-@end
-@implementation PodsDummy_Pods_RNYubikit
-@end
--- ios/Pods/Target Support Files/YubiKit/YubiKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_YubiKit : NSObject
-@end
-@implementation PodsDummy_YubiKit
-@end
