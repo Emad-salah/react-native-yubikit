73b42af41ae7d443496d9dba664e713f8b8d786a
emad-salah <emads14@gmail.com>
2020-01-29 19:29:04 +0100

    Bug fixes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { NativeModules } from 'react-native';
+import { NativeModules } from "react-native";
 
-const { Yubikit } = NativeModules;
+const { RNYubikit } = NativeModules;
 
-export default Yubikit;
+export default RNYubikit;
